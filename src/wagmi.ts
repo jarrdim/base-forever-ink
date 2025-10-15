562699ea832d1a89e411f833242b33f7c3c9ffae
jarrdim <jackmutiso37@gmail.com>
2025-10-15 15:17:55 +0300

    added the project ID

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -3,7 +3,7 @@ import { base, baseSepolia } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'Base Forever Book',
-  projectId: 'YOUR_WALLETCONNECT_PROJECT_ID', // Get this from https://cloud.walletconnect.com/
+  projectId: '9bf0183349252b3da5a51e76f76d6761', // Get this from https://cloud.walletconnect.com/
   chains: [base, baseSepolia],
   ssr: false,
 });
